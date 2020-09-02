f6f15d313c9cf13242dc38d9753423a8ab00e707
Igor-Felipy <igor.felipy123@gmail.com>
2020-09-02 16:05:30 -0400

    testando rotas

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,15 @@
 from app import app
 
-
+@app.route("/index")
 @app.route("/")
 def index():
     return "Hello World!"
+
+
+@app.route("/teste/", defaults={'name':None})
+@app.route("/teste/<name>")
+def teste(name):
+    if name:
+        return "Olá, %s!" % name
+    else:
+        return "Olá, Usuario!"
