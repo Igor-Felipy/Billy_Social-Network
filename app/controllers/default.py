d5248cd27fd59cd79d8558beeb286efa57e9f4e9
Igor-Felipy <igor.felipy123@gmail.com>
2020-09-03 23:34:34 -0400

    criando forms

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 
+from app.models.forms import LoginForm
 
 @app.route("/index/")
 @app.route("/")
@@ -8,6 +9,12 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/login/")
+@app.route("/login/", methods=['GET','POST'])
 def login():
-    return render_template('base.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else:
+        print(form.errors)
+    return render_template('login.html', form=form)
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+SECRET_KEY = 'um-nome-bem-seguro'
